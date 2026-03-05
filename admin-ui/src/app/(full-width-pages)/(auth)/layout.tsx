54cf1e5ce3c979ed25f9f20297385e556bf97b7c
feno-tolojanahary <fenoheriniainat@gmail.com>
2026-03-05 10:08:25 +0300

    simple signing plage

--- admin-ui/src/app/(full-width-pages)/(auth)/layout.tsx
+++ admin-ui/src/app/(full-width-pages)/(auth)/layout.tsx
@@ -14,27 +14,11 @@ export default function AuthLayout({
   return (
     <div className="relative p-6 bg-white z-1 dark:bg-gray-900 sm:p-0">
       <ThemeProvider>
-        <div className="relative flex lg:flex-row w-full h-screen justify-center flex-col  dark:bg-gray-900 sm:p-0">
-          {children}
-          <div className="lg:w-1/2 w-full h-full bg-brand-950 dark:bg-white/5 lg:grid items-center hidden">
-            <div className="relative items-center justify-center  flex z-1">
-              {/* <!-- ===== Common Grid Shape Start ===== --> */}
-              <GridShape />
-              <div className="flex flex-col items-center max-w-xs">
-                <Link href="/" className="block mb-4">
-                  <Image
-                    width={231}
-                    height={48}
-                    src="./images/logo/auth-logo.svg"
-                    alt="Logo"
-                  />
-                </Link>
-                <p className="text-center text-gray-400 dark:text-white/60">
-                  Free and Open-Source Tailwind CSS Admin Dashboard Template
-                </p>
-              </div>
-            </div>
+        <div className="relative flex h-screen w-full items-center justify-center dark:bg-gray-900 sm:p-0">
+          <div className="absolute inset-0 hidden lg:block">
+            <GridShape />
           </div>
+          <div className="relative z-10 w-full">{children}</div>
           <div className="fixed bottom-6 right-6 z-50 hidden sm:block">
             <ThemeTogglerTwo />
           </div>
